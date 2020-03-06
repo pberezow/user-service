b4ad0bda72bd9e8925fa2a3e0ecd30d3b4bc4a58
Piotr Berezowski <piotr.berezowski97@gmail.com>
2020-03-06 20:47:56 +0100

    Add initadmin cmd

--- user/apps.py
+++ user/apps.py
@@ -5,18 +5,18 @@ from time import sleep
 class UserConfig(AppConfig):
     name = 'user'
 
-    def ready(self):
-        from py_eureka_client import eureka_client
-        from user_service.settings import EUREKA
-
-        eureka_up = False
-        while not eureka_up:
-            try:
-                eureka_client.init(
-                    eureka_server=EUREKA['HOST'],
-                    app_name='user-service',
-                    instance_port=int(EUREKA['DOCKER_PORT'])
-                )
-                eureka_up = True
-            except Exception as e:
-                sleep(2.0)
+    # def ready(self):
+    #     from py_eureka_client import eureka_client
+    #     from user_service.settings import EUREKA
+    #
+    #     eureka_up = False
+    #     while not eureka_up:
+    #         try:
+    #             eureka_client.init(
+    #                 eureka_server=EUREKA['HOST'],
+    #                 app_name='user-service',
+    #                 instance_port=int(EUREKA['DOCKER_PORT'])
+    #             )
+    #             eureka_up = True
+    #         except Exception as e:
+    #             sleep(2.0)
---
+++ user/management/commands/initadmin.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from user.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if User.objects.filter(username='admin') == 0:
+            print('Creating admin account with credentials: admin / admin123')
+            data = {
+                'username': 'admin',
+                'password': 'admin123',
+                'licence_id': 1,
+                'email': 'wojtczitolamus@gmail.com',
+                'is_admin': True
+            }
+            admin = User.objects.create_superuser(**data)
+            # for user in settings.ADMINS:
+            #     username = user[0].replace(' ', '')
+            #     email = user[1]
+            #     password = 'admin'
+            #     print('Creating account for %s (%s)' % (username, email))
+            #     admin = Account.objects.create_superuser(email=email, username=username, password=password)
+            #     admin.is_active = True
+            #     admin.is_admin = True
+            #     admin.save()
+        else:
+            print('Admin accounts can only be initialized if no Accounts exist')
