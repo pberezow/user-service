dcbb61f35aef3693a5227aec9ffe764850d7bf8e
Piotr Berezowski <piotr.berezowski97@gmail.com>
2020-08-30 16:07:59 +0200

    Remove old Django-related files

--- group/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from group.views import GroupDetailsView, GroupListCreateView
-
-
-urlpatterns = [
-    path('', GroupListCreateView.as_view(), name='group-list-create'),
-    path('<str:group_name>/', GroupDetailsView.as_view(), name='group-details'),
-]
--- user/views/otherUserViews.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.generics import ListAPIView
-from rest_framework.response import Response
-
-
-class HealthCheckView(ListAPIView):
-    def list(self, request, *args, **kwargs):
-        return Response({'status': 'UP'})
