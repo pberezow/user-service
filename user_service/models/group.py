a783c5523d725410f6a98c4a733546f6c845eba3
Piotr Berezowski <piotr.berezowski97@gmail.com>
2020-09-07 10:20:42 +0200

    Add user and group transport objects (models)

--- user_service/models/group.py
+++ user_service/models/group.py
@@ -1,7 +1,5 @@
-# id = Column(Integer, primary_key=True)
-# licence_id = Column(Integer, nullable=False)
-# name = Column(String(30), nullable=False)
-# users = relationship('User', secondary=users_groups, back_populates='groups')
+from typing import Dict, Tuple, Any
+
 
 class GroupTO:
     def __init__(self, id, licence_id, name, users=None):
@@ -12,3 +10,20 @@ class GroupTO:
 
     def add_user(self, user_to):
         self.users.append(user_to)
+
+    def as_dict(self) -> Dict[str, Any]:
+        # return UserTO as Dict
+        return {
+            'id': self.id,
+            'licence_id': self.licence_id,
+            'name': self.name,
+            'users': [u.as_dict() for u in self.users]
+        }
+
+    def as_json(self) -> Dict[str, Any]:
+        return {
+            'id': self.id,
+            'licence_id': self.licence_id,
+            'name': self.name,
+            'users': [u.as_json() for u in self.users]
+        }
--- user_service/models/user.py
+++ user_service/models/user.py
@@ -1,26 +1,18 @@
-# id = Column(sqlalchemy.Integer, primary_key=True)  # SET TO UUID???
-# licence_id = Column(Integer, nullable=False)
-# username = Column(String(150), nullable=False)
-# password = Column(String(128), nullable=False)
-# is_admin = Column(Boolean, default=False),
-# first_name = Column(String(30), nullable=False)
-# last_name = Column(String(150), nullable=False)
-# email = Column(String(120), nullable=False, unique=True)
-# phone_number = Column(String(12), default=None)
-# address = Column(String(100), default=None)
-# position = Column(String(30), nullable=False)
-# last_login = Column(DateTime, nullable=False, default=datetime.now())
-# date_joined = Column(DateTime, nullable=False, default=datetime.now())
-# is_active = Column(Boolean, default=True)
-# groups
+from typing import Dict, Any
+from datetime import datetime
 
-class UserTO:
-    def __init__(self, id, licence_id, username, password, email, is_admin, first_name, last_name, phone_number,
-                 address, position, last_login, date_joined, is_active, groups=None):
+from user_service.utils.base_to import BaseTO
+from user_service.models.group import GroupTO
+
+
+class UserTO(BaseTO):
+
+    def __init__(self, id=None, licence_id=None, username=None, password=None, email=None, is_admin=None, first_name=None, last_name=None, phone_number=None,
+                 address=None, position=None, last_login=None, date_joined=None, is_active=None, groups=None):
         self.id = id
         self.licence_id = licence_id
         self.username = username
-        self.password = password
+        self.password = password  # bcrypt hash
         self.email = email
         self.is_admin = is_admin
         self.first_name = first_name
@@ -32,6 +24,50 @@ class UserTO:
         self.date_joined = date_joined
         self.is_active = is_active
         self.groups = groups or []
+        super().__init__()
 
-    def add_group(self, group_to):
+    def __repr__(self):
+        return f'User: {self.id}, {self.username}, {self.email} '
+
+    def add_group(self, group_to: GroupTO):
         self.groups.append(group_to)
+
+    def as_dict(self) -> Dict[str, Any]:
+        # return UserTO as Dict
+        return {
+            'id': self.id,
+            'licence_id': self.licence_id,
+            'username': self.username,
+            'password': self.password,
+            'email': self.email,
+            'is_admin': self.is_admin,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'phone_number': self.phone_number,
+            'address': self.address,
+            'position': self.position,
+            'last_login': self.last_login,
+            'date_joined': self.date_joined,
+            'is_active': self.is_active,
+            'groups': [g.as_dict() for g in self.groups]
+        }
+
+    def as_json(self) -> Dict[str, Any]:
+        # return UserTO as Dict, which can be an input of json.dumps() - no datetime
+        return {
+            'id': self.id,
+            'licence_id': self.licence_id,
+            'username': self.username,
+            'password': self.password,
+            'email': self.email,
+            'is_admin': self.is_admin,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'phone_number': self.phone_number,
+            'address': self.address,
+            'position': self.position,
+            'last_login': datetime.timestamp(self.last_login) if self.last_name else None,
+            'date_joined': datetime.timestamp(self.date_joined),
+            'is_active': self.is_active,
+            'groups': [g.as_json() for g in self.groups]
+        }
