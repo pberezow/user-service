c4832b240c5c614adbba1054aaef96da21567653
Piotr Berezowski <piotr.berezowski97@gmail.com>
2020-09-07 10:24:11 +0200

    Add user service for CRUD operations on users table

--- user_service/services/user_crud_service.py
+++ user_service/services/user_crud_service.py
@@ -1,42 +1,25 @@
-import bcrypt
-import jwt
-from typing import List, Dict, Optional, Any
-from datetime import datetime as dt
+from typing import List, Optional
+
 from user_service.models.user import UserTO
-from user_service.models.exceptions import ValidationException
 from user_service.repository.user_repository import UserRepository
-from user_service.repository.exceptions import DatabaseException, UserDoesNotExist
+from user_service.exceptions.database import DatabaseException, UserDoesNotExist
+from user_service.utils.password_utils import hash_password
 
 
-class UserService:
+class UserCRUDService:
     def __init__(self, user_repository: UserRepository):
         self._user_repository = user_repository
 
-    def create_user(self, licence_id: int, username: str, password: str, is_admin: bool, first_name: str,
-                    last_name: str, email: str, phone_number: str, address: str, position: str) -> UserTO:
+    def create_user(self, user_to: UserTO) -> UserTO:
         """
         Creates new user.
-        Returns UserTO or raises ValidationException
+        Returns UserTO or None if does not exist in database.
         """
-        hash_password = self._hash_password(password)
-        user_to = UserTO(licence_id=licence_id,
-                         username=username,
-                         password=hash_password,
-                         is_admin=is_admin,
-                         first_name=first_name,
-                         last_name=last_name,
-                         email=email,
-                         phone_number=phone_number,
-                         address=address,
-                         position=position,
-                         is_active=True,
-                         date_joined=dt.now(),
-                         last_login=None
-                         )
+        hashed_pwd = hash_password(user_to.password)
+        user_to.password = hashed_pwd
         try:
-            user_to.validate(skip_none_values=True)
             user_to = self._user_repository.insert_user(user_to)
-        except DatabaseException or ValidationException as err:
+        except DatabaseException as err:
             # TODO - add falcon's errors
             raise err
 
@@ -50,43 +33,42 @@ class UserService:
         users = self._user_repository.get_users_by_licence_id(licence_id)
         return users
 
-    def get_user(self, licence_id: int, username: str) -> UserTO:
+    def get_user(self, licence_id: int, username: str) -> Optional[UserTO]:
         """
         Get user with matching licence_id and username.
-        Returns user TO or raises Exception
+        Returns user TO or None if user does not exist.
         """
         try:
             user_to = self._user_repository.get_user_by_username(username, licence_id)
-        except DatabaseException as err:
-            raise err
+        except UserDoesNotExist as err:
+            return None
 
         return user_to
 
     def set_user_data(self):
-
         pass
 
-    def remove_user(self, licence_id: int, username: str) -> UserTO:
+    def remove_user(self, licence_id: int, username: str) -> Optional[UserTO]:
         """
         Delete user.
-        Returns TO for removed user.
+        Returns TO for removed user or None if user does not exist.
         """
         try:
             user_to = self._user_repository.delete_user_by_username(username, licence_id)
-        except DatabaseException as err:
-            raise err
+        except UserDoesNotExist as err:
+            return None
 
         return user_to
 
-    def restore_user(self, licence_id: int, username: str) -> UserTO:
+    def restore_user(self, licence_id: int, username: str) -> Optional[UserTO]:
         """
         Restore deleted user.
-        Returns TO for restored user.
+        Returns TO for restored user or None if user does not exist.
         """
         try:
             user_to = self._user_repository.restore_user_by_username(username, licence_id)
-        except DatabaseException as err:
-            raise err
+        except UserDoesNotExist as err:
+            return None
 
         return user_to
 
