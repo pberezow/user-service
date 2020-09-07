97b84cfab8b3b49de94576c7eb1d64775b322fc0
Piotr Berezowski <piotr.berezowski97@gmail.com>
2020-09-07 14:11:06 +0200

    Fix AuthMiddleware

--- user_service/middlewares/auth_middleware.py
+++ user_service/middlewares/auth_middleware.py
@@ -1,15 +1,13 @@
-import jwt
-import os
 import falcon
 from falcon import Request, Response
 from typing import Optional, Set
-from user_service.services.user_crud_service import UserService
+from user_service.services.jwt_service import JWTService
 
 
 class AuthMiddleware:
 
-    def __init__(self, user_service: UserService, allowed_paths: Optional[Set[str]] = None):
-        self._user_service = user_service
+    def __init__(self, jwt_service: JWTService, allowed_paths: Optional[Set[str]] = None):
+        self._jwt_service = jwt_service
         self._allowed_paths = allowed_paths or set()
 
     def process_request(self, req: Request, resp: Response):
@@ -21,7 +19,7 @@ class AuthMiddleware:
             # no token provided
             raise falcon.HTTPUnauthorized()
 
-        user_to = self._user_service.get_user_from_jwt(token)
+        user_to = self._jwt_service.get_user_from_jwt(token)
         if user_to is None:
             # invalid token
             raise falcon.HTTPUnauthorized()
